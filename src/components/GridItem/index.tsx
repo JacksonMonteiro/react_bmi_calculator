a46a7dacee21b7291ceb520ffecb3f81258d3507
jacksonmonteiro <infor.jackson324@gmail.com>
2022-03-18 13:43:07 -0300

    Right side

---
+++ src/components/GridItem/index.tsx
@@ -0,0 +1,13 @@
+import { Level } from "../../helpers/imc";
+
+type Props = {
+    data: Level
+}
+
+const GridItem = ({ data }: Props) => {
+    return(
+        <div>...</div>
+    );
+}
+
+export default GridItem;
--- src/components/Rightside/index.tsx
+++ src/components/Rightside/index.tsx
@@ -1,9 +1,16 @@
 import styles from './styles.module.css';
 
+import { levels } from '../../helpers/imc';
+import GridItem from '../GridItem';
+
 const RightSide = () => {
     return(
         <div className={styles.right_side}>
-            ...
+            <div className={styles.grid}>
+                {levels.map((item, key) => (
+                    <GridItem key={key} data={item} />
+                ))}
+            </div>
         </div>
     );
 }
